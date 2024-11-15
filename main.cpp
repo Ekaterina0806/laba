b2636a1472436762f76f9af0f180d35de88da79d
Ekaterina0806 <katgil055@gmail.com>
2024-11-15 12:44:33 +0500

    Попытка 1000

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
+vector<string> read_strings(const string& filename)
 {
-    cout << "Hello world!" << endl;
+    vector<string> strings;
+    // Здесь будет код для чтения строк из файла
+
+    return strings;
+}
+
+// Функция для вывода строк на экран
+void print_strings(const vector<string>& strings)
+{
+    // Здесь будет код для вывода строк на экран
+}
+
+// Функция для записи строк в файл
+void write_strings(const vector<string>& strings, const string& filename)
+{
+    // Здесь будет код для записи строк в файл
+}
+
+int main() {
+    string filename = "input.txt";
+    vector<string> strings = read_strings(filename);
+    print_strings(strings);
+    write_strings(strings, filename);
     return 0;
 }
